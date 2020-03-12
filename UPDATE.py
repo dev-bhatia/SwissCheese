f87cc9c7dfbeb298b3ba0231dc9bfc04edd87cb3
Dev Bhatia <32446285+dev-bhatia@users.noreply.github.com>
2020-03-12 12:44:38 -0700

    Updated Updater Script

--- UPDATE.py
+++ UPDATE.py
@@ -1,27 +1,20 @@
 """
 A quick script executed before RUNME.py on our automation host to ensure
-we are running the most up-to-date stable build.
+    we are running the most up-to-date stable build.
 """
 import os
 import sys
 
 def execute_command(cmd):
-    
+    try:
+        print("Attempting to run: {}".format(cmd))
+        os.system(cmd)
+        print("Completed running: {}".format(cmd))
+    except Exception as e:
+        print("Encountered Error Running: {}".format(cmd))
+        print("Exception: {}".format(e))
+        sys.exit(1)
 
 # Update branch to latest version
-try:
-    cmd = "git fetch"
-    print("Updating branch to latest version if necessary...")
-    os.system(cmd)
-    print("Completed Fetch. Attempting Reset...")
-except Exception as e:
-    print("Error in git fetch: {}".format(e))
-    sys.exit(1)
-
-try:
-    print("Attempting Hard Reset")
-    os.system("git reset --hard origin/master")
-    print("Completed Reset. Program Updated")
-except Exception as e:
-    print("Error in git reset --hard origin/master: {}".format(e))
-    sys.exit(1)
+execute_command("git fetch")
+execute_command("git reset --hard origin/master")
